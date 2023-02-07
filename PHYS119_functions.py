9a4581f298af1f0c48a60f6952511d1e036ed78e
LegendWeasel <44554087+LegendWeasel@users.noreply.github.com>
2023-02-06 19:31:44 -0800

    Add files via upload
    Updated for Lab3

--- PHYS119_functions.py
+++ PHYS119_functions.py
@@ -2,6 +2,7 @@ import numpy as np
 
 #Please make sure all of your inputed data is in SI units (m,kg,s, etc)
 #All inputs are assumed to be scalars(a number) unless explicitly stated otherwise
+#"Standard" font for labels
 
 g = 9.809 #Acceleration due to gravity
 
@@ -19,7 +20,9 @@ g = 9.809 #Acceleration due to gravity
 #uA: u[A], uncertainty of A
 #uB: u[B], uncertainty of B
 def calc_t_prime(A, B, uA, uB):
-    return np.abs(A-B)/(np.sqrt(uA**2 + uB**2))
+    t_prime = np.abs(A-B)/(np.sqrt(uA**2 + uB**2))
+    print("t' = ", t_prime)
+    return t_prime
 
 #Calculates the uncertainty of R(results) assuming R = aX +bY +c given:
 #a,b,c: Constants, values with no uncertainty
@@ -62,13 +65,57 @@ def calc_uK(k, um1, um2, ux1, ux2):
     #Calculates u[deltaM] and u[deltaX]
     u_delta_m = ((um1)**2 + (um2)**2)**0.5
     u_delta_x = ((ux1)**2 + (ux2)**2)**0.5
+    print("u[Δm] = ", u_delta_m)
+    print("u[Δx] = ", u_delta_x)
 
     u_delta_f = g * u_delta_m #Calculates u[deltaF]
+    print("u[Δf] = ", u_delta_f)
 
     #Calculates deltaF and deltaX
     delta_f = g * np.abs(m1-m2)
     delta_x = np.abs(x1-x2)
+    print("Δf = ", delta_f)
+    print("Δx = ", delta_x)
 
     #Calculates and returns the sigmaF value
-    return k * ((u_delta_f/delta_f)**2 + (u_delta_x/delta_x)**2)**0.5
+    u_k = k * ((u_delta_f/delta_f)**2 + (u_delta_x/delta_x)**2)**0.5
+    print("u[k] = ", u_k)
+    return u_k
 
+#   ____       __                _   _             
+#  |  _ \ ___ / _|_ __ __ _  ___| |_(_) ___  _ __  
+#  | |_) / _ \ |_| '__/ _` |/ __| __| |/ _ \| '_ \ 
+#  |  _ <  __/  _| | | (_| | (__| |_| | (_) | | | |
+#  |_| \_\___|_| |_|  \__,_|\___|\__|_|\___/|_| |_|
+
+#Calculates a (diameter of hair) given:
+#p: A given dark minimum, p = {1,2,3,...} pick 1 number
+#lamda: Wavelength of light shone
+#L: Length between hair and paper/surface
+#y: Distance between center of central bright fringe and the given dark minimum in accordance to p
+def calc_a_diameter(p, lambdaVal, L, y):
+    z = L/y #equal to cotangent(theta)
+    print("z = ", z)
+
+    a = p * lambdaVal * (1 + z**2) ** 0.5
+    print("a = ", a)
+    return 
+
+#Calculates u[a] given:
+#a: Diameter of hair
+#L: Length between hair and paper/surface
+#y: Distance between center of central bright fringe and the given dark minimum in accordance to p
+#u_L: u[L], Uncertainty of L
+#u_y: u[y], Uncertainty of y
+def calc_u_a(p,L, y, u_L, u_y, lambdaVal):
+    z = L/y
+    print("z = ", z)
+
+    u_z = z * ((u_L/L)**2 + (u_y/y)**2) ** 0.5 #calcs u[z]
+    print("u[z] = ", u_z)
+
+    u_a = ((p * lambdaVal * u_z * z) / (1 + z**2) ** 0.5) #calcs and prints u[a]
+    print("u[a] = ",u_a)
+
+    return u_a
+                                                 
